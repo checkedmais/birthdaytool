0d0dab77073b79ba339c381c03c0805a4b7a3430
Alex Oertel <checkedmais@gmail.com>
2021-05-17 23:45:05 +0200

    started to write main

--- main.cpp
+++ main.cpp
@@ -1,21 +1,71 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "date.h"
 #include "person.h"
 #include "personlist.h"
 #include "personlistiterator.h"
 
+void printHelp();
+
 int main(int argc, char **argv) {
-   // std::cout << "Hello, world!" << std::endl;
-  // Person alex("alex", Date(7,2,2001), true);
-  // // std::cout<<alex.getName()<<alex.getBirthday();
-    PersonList list("dates");
-    for(auto it = list.begin(); it != list.end(); ++it) 
+    std::string fileName = "dates";
+    bool fileSet = false;
+ /*   if(argc == 1)  //no arguments, standard procedure
     {
-        std::cout<<(*it).getName()<<(*it).getBirthday()<<std::endl;
+        PersonList list("dates"); //standard file
+        for(auto it = list.begin(); it != list.end(); ++it) //cycle through the birthdays
+        {                                                   //PersonListIterator ensures that the order of dates is good
+            std::cout<<(*it).getName()<<(*it).getBirthday()<<std::endl;
+        }   
+        return 0;
+    } */
+    
+    for(int i = 1; i < argc; i++)
+    {
+        std::string arg = argv[i];
+        if( (arg == "-h") || (arg =="--help") )
+        {
+            printHelp();
+            return 0;
+        }
+        if( (arg == "-f") || (arg == "--file") )
+        {
+            if ((i + 1 >= argc) || fileSet)
+            {
+                printHelp();
+                return 0;
+            } //no file selected or a file has already been selected
+            
+            fileName = argv[++i];
+            
+            //TODO: check for bad fileNames
+            
+            continue;
+        }
+        
+        if( (arg == "-a") || (arg == "--add") )
+        {
+            if(i + 2 >= argc) //nothing to add there
+            {
+                printHelp();
+                return 0;
+            }
+            //TODO: add person
+            
+            std::string name_helper = argv[i+1];
+            std::string date_helper = argv[i+2];
+        }
     }
-    list.save(); 
+    PersonList list(fileName); //TODO: check for emptyness
+    for(auto it = list.begin(); it != list.end(); ++it) //cycle through the birthdays
+    {                                                   //PersonListIterator ensures that the order of dates is good
+        std::cout<<(*it).getName()<<(*it).getBirthday()<<std::endl;
+    }  
     return 0;
 }
 
-
-//todo: create PersonList class, which handels sorting, printing, etc
+void printHelp()
+{
+    std::cerr<<"help"<<std::endl;
+}
